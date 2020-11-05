31a4698279005b68c28a69b9fdb28379bad1189a
sarathaoho <sarathaoho@users.noreply.github.com>
2020-11-05 13:40:58 +0100

    test

---
+++ GUI/Admin/UserAndMechanic/UserAndMechanic.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI.Admin.UserOrMechanic
+{
+    /// <summary>
+    /// Interaction logic for UserAndMechanic.xaml
+    /// </summary>
+    public partial class UserAndMechanic : Page
+    {
+        public UserAndMechanic()
+        {
+            InitializeComponent();
+        }
+
+        private void AddRemoveUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddRemoveMechanic_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void InfoMechanic_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddRemoveSkill_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
